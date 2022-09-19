269df09ae4b38bd4e6943be4479b117b79c1a84a
mohanrajcse96 <mohan@sparkouttech.com>
2022-09-19 18:17:04 +0530

    Created Wekta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-
+  BASE_URL:'https://devpi.ektanft.io/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
